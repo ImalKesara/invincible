768e9fd8dee39ad3e8adc99d90dd7414724ddc19
LoneWolf <imalkesara2001@gmail.com>
2024-03-31 22:48:14 +0530

    => + chatgpt

--- src/Cardlist.js
+++ src/Cardlist.js
@@ -1,20 +1,33 @@
 import React from "react";
 import Card from "./Card";
 
+// const Cardlist = ({ robots }) => {
+//   return (
+//     <div>
+//       {robots.map((robot) => (
+//         <Card
+//           key={robot.id}
+//           id={robot.id}
+//           name={robot.name}
+//           username={robot.username}
+//           email={robot.email}
+//         />
+//       ))}
+//     </div>
+//   );
+// };
 const Cardlist = ({ robots }) => {
-  return (
-    <div>
-      {robots.map((robot) => (
-        <Card
-          key={robot.id}
-          id={robot.id}
-          name={robot.name}
-          username={robot.username}
-          email={robot.email}
-        />
-      ))}
-    </div>
-  );
+  const cardComponent = robots.map((user, i) => {
+    return (
+      <Card
+        id={robots[i].id}
+        name={robots[i].name}
+        username={robots[i].username}
+        email={robots[i].email}
+      />
+    );
+  });
+  return <div>{cardComponent}</div>;
 };
 
 export default Cardlist;
